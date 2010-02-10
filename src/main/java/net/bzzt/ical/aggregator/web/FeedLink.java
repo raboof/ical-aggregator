c0a98340ea3deb6c8aee7c8af3ada287179781d8
Arnout Engelen <arnouten@localhost.(none)>
2010-02-10 22:59:13 +0100

    link to specific feeds

---
+++ src/main/java/net/bzzt/ical/aggregator/web/FeedLink.java
@@ -0,0 +1,48 @@
+package net.bzzt.ical.aggregator.web;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.bzzt.ical.aggregator.model.Feed;
+import net.bzzt.ical.aggregator.web.model.JpaEntityModel;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.wicket.Session;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.panel.Panel;
+
+import edu.emory.mathcs.backport.java.util.Collections;
+
+public class FeedLink extends Panel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public FeedLink(String id, Feed feed) {
+		super(id);
+	
+		Link<Feed> link = new Link<Feed>("feed", new JpaEntityModel<Feed>(feed))
+		{
+
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void onClick() {
+				List<Feed> feedList = new ArrayList<Feed>();
+				feedList.add(getModelObject());
+				((AggregatorSession) Session.get()).setSelectedFeeds(feedList);
+				setResponsePage(HomePage.class);
+			}
+			
+		};
+		link.add(new Label("shortName", feed.shortName));
+		add(link);
+	}
+
+}
