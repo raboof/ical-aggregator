89b2d748814733d6a560ab2a0ff27b69dee33c6e
Arnout Engelen <arnouten@localhost.(none)>
2010-03-03 21:13:09 +0100

    next/previous week

--- src/main/java/net/bzzt/ical/aggregator/web/WeekView.java
+++ src/main/java/net/bzzt/ical/aggregator/web/WeekView.java
@@ -3,6 +3,10 @@ package net.bzzt.ical.aggregator.web;
 import java.util.Calendar;
 import java.util.Date;
 
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.model.Model;
+
 public class WeekView extends AggregatorLayoutPage
 {
 	public WeekView()
@@ -10,9 +14,49 @@ public class WeekView extends AggregatorLayoutPage
 		this(new Date());
 	}
 
-	public WeekView(Date date)
+	public WeekView(final Date date)
 	{
 		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(date);
+		
+		add(new Link<Void>("previous")
+		{
+
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void onClick()
+			{
+				Calendar calendar = Calendar.getInstance();
+				calendar.setTime(date);
+				calendar.add(Calendar.WEEK_OF_YEAR, -1);
+				setResponsePage(new WeekView(calendar.getTime()));
+			}
+			
+		});
+		add(new Label("week", new Model<Integer>(calendar.get(Calendar.WEEK_OF_YEAR))));
+		add(new Link<Void>("next")
+		{
+
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void onClick()
+			{
+				Calendar calendar = Calendar.getInstance();
+				calendar.setTime(date);
+				calendar.add(Calendar.WEEK_OF_YEAR, 1);
+				setResponsePage(new WeekView(calendar.getTime()));
+			}
+			
+		});
+		
 		calendar.add(Calendar.DAY_OF_MONTH, 2-calendar.get(Calendar.DAY_OF_WEEK));
 		add(new DayPanel("monday", calendar.getTime()));
 		
