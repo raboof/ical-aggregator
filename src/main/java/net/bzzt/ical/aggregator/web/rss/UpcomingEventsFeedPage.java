f195110df9527af74fd7ccd4ea6bc534f884a5ae
Arnout <arnouten@bzzt.net>
2010-05-16 22:26:55 +0200

    set the 'guid' to something unique per event (database-id).

--- src/main/java/net/bzzt/ical/aggregator/web/rss/UpcomingEventsFeedPage.java
+++ src/main/java/net/bzzt/ical/aggregator/web/rss/UpcomingEventsFeedPage.java
@@ -30,13 +30,16 @@ public class UpcomingEventsFeedPage extends FeedPage
 		{
 			Event event = (Event) rawEvent;
 			
-			SyndEntry entry = new SyndEntryImpl();
+			SyndEntryImpl entry = new SyndEntryImpl();
 			entry.setTitle(event.summary);
 			if (event.url != null)
 			{
 				entry.setLink(event.url.toExternalForm());
 			}
 			entry.setPublishedDate(event.getStart());
+
+			// This will be the guid: http://wiki.java.net/bin/view/Javawsxml/Rome05URIMapping
+			entry.setUri(WicketApplication.getLink() + "/event/" + event.id);
 			
 			SyndContent description = new SyndContentImpl();
 			description.setType("text/plain");
